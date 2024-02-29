8c27a0f9743f149363fc341f5dd302c1b20c3937
Eric T <erict@Erics-MacBook-Air-5.local>
2024-02-29 14:18:16 -0500

    files

---
+++ our_app.js
@@ -0,0 +1,16 @@
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+createApp({ 
+    data() {
+        return { 
+        }; 
+    },
+    watch:{ 
+    },
+    methods: {  
+    }
+        
+}).mount('#app')
+
+window.myVueApp = app;
+    
