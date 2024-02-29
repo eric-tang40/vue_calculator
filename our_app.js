3f58c5a81cb4fe19ad9aad88017a00afb191e604
Eric T <erict@Erics-MacBook-Air-5.local>
2024-02-29 14:50:36 -0500

    work

--- our_app.js
+++ our_app.js
@@ -2,12 +2,26 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
 createApp({ 
     data() {
-        return { 
+        return {
+            field: "",
         }; 
     },
     watch:{ 
     },
     methods: {  
+        addValue(x) {
+            this.field += x;
+        }
+        calculate() {
+            for(int i=0; i<this.field.length(); i++) {
+                if(this.field[i].isNaN) {
+                    
+                }
+                else {
+
+                }
+            }
+        }
     }
         
 }).mount('#app')
