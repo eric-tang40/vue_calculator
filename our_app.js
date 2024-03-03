0e3bf35a29a2264b5fb3898504be5592823f6246
Eric T <erict@Erics-MacBook-Air-5.local>
2024-03-02 21:01:33 -0500

    eric new changes

--- our_app.js
+++ our_app.js
@@ -3,25 +3,50 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({ 
     data() {
         return {
-            field: "",
+            display: "", // what the user will see in the input field
+            backend: "", // what we use to calculate
+            result: "",
         }; 
     },
     watch:{ 
     },
     methods: {  
         addValue(x) {
-            this.field += x;
-        }
-        calculate() {
-            for(int i=0; i<this.field.length(); i++) {
-                if(this.field[i].isNaN) {
-                    
-                }
-                else {
-
-                }
+            this.display += x;
+            if(x == "÷") { // various if statements to make sure there are no errors in evaluating 
+                this.backend += '/';
             }
-        }
+            else if(x == "π") {
+                this.backend += 'Math.PI';
+            }
+            else if(x == 'mod') {
+                this.backend += '%';
+            }
+            else if(x == '%') {
+                this.backend += '/100';
+            }
+            else if(x == '^') {
+                this.backend += '**';
+            }
+            else {
+                this.backend += x;
+            }
+        },
+        calculate() {
+            try {
+                this.result = eval('(' + this.backend + ')').toString(); 
+                // eval() command is able to calculate certain strings 
+            } 
+            catch (error) {
+              this.result = "Error. Not a proper mathematical function.";
+            }
+        },
+        clearField() {
+            this.display = "";
+            this.backend = "";
+            this.result = "";
+        },
+        
     }
         
 }).mount('#app')
